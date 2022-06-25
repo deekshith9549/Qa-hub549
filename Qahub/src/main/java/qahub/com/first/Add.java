7d7c5509873905b1431f5bc0a248652b25a96eb7
Deekshith <seniortestengineer4@gmail.com>
2022-06-25 14:03:02 +0530

    Added Add class

---
+++ Qahub/src/main/java/qahub/com/first/Add.java
@@ -0,0 +1,12 @@
+package qahub.com.first;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8;
+		int b=9;
+		System.out.println(a+b);
+	}
+
+}
